8a36064bb0663a58b56bc1cf9839ca6ec94e27cd
Jarek Kajko <jaruto7@gmail.com>
2018-09-26 17:30:00 +0200

    Create server requests to read & write test.json file in server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+var fs = require( 'fs' );
+var express = require( 'express' );
+var bodyParser = require( 'body-parser' );
+var app = express();
+var stringyFile;
+// Uzyj body-parser aby skorzystac z formatu application/json
+app.use( bodyParser.json());
+// Wywolaj metode GET ktora pobierze dane z serwera jesli endpoint zostanie znaleziony
+app.get( '/getNote', function( req, res ) {
+    // Wywolaj metode ktora zczyta plik w Node po przeslaniu endpointa
+    fs.readFile( './test.json', 'utf8', function( err, data ) {
+        // Jesli plik nie zostal znaleziony, zwroc blad i wyswietl w Node
+        if( err ) throw err;
+        // Przypisz parametr ktory przechowuje zapytanie o endpoincie zapisane jako string
+        stringyFile = data;
+        // Wyslij zapytanie do serwera
+        res.send( data );
+    });
+});
+
+// Wywolaj metode POST ktora przesle dane do serwera jesli dynamiczny endpoint zostal odszukany
+app.post( '/updateNote/:note', function( req, res ) {
+    // Przypisz obiekt przechowujacy zapytanie o endpoint do zmiennej
+    stringyFile = req.params.note;
+    // Wywolaj metode ktora zapisze pobrane dane z endpointa do pliku json
+    fs.writeFile( './test.json', stringyFile, function( err ) {
+        // Jesli plik nie zostal znaleziony, wyswietl blad w Node
+        if( err ) throw err;
+        // Powiadom serwer ze plik zostal pomyslnie zaktualizowany
+        console.log( 'file updated' );
+    });
+});
+
+// Nasluchuj zmiany na port serwera
+app.listen( 3000 );
